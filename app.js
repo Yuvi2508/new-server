bc4e9257715d2e4b176cb8b64c60d89b463db578
Yuvi2508 <azaria1471@gmail.com>
2023-03-23 18:08:07 +0200

    first

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ import express from "express";
 
 const server = express();
 
+const port = process.env.PORT || 3000
+
+
 server.use('*', (req, res, next) => {
       res.send('hello')
 })
 
 
-server.listen(3000, () => {
-      console.log(`lisening on port ${3000}`);
+server.listen(port, () => {
+      console.log(`lisening on port ${port}`);
 })
